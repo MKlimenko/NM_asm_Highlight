fb69924fcd6f18ec93d4e687a1de7e90da98980e
Mikhail Klimenko <m.klimenko@module.ru>
2016-08-25 14:55:23 +0300

    Ver 0.2.2
    	Перевод к чистовому варианту, промежуточный коммит

--- Classification/ClassificationFormat.cs
+++ Classification/ClassificationFormat.cs
@@ -6,21 +6,13 @@ using Microsoft.VisualStudio.Utilities;
 namespace NM_asm_Language
 {
     #region Format definition
-    /// <summary>
-    /// Defines the editor format for the ookExclamation classification type. Text is colored BlueViolet
-    /// </summary>
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = "nm_asm_keyword")]
     [Name("nm_asm_keyword")]
-    //this should be visible to the end user
     [UserVisible(false)]
-    //set the priority to be after the default classifiers
     [Order(Before = Priority.Default)]
     internal sealed class NM_asm_Keyword : ClassificationFormatDefinition
     {
-        /// <summary>
-        /// Defines the visual format for the "exclamation" classification type
-        /// </summary>
         public NM_asm_Keyword()
         {
             DisplayName = "NM asm Keyword"; //human readable version of the name
@@ -28,21 +20,13 @@ namespace NM_asm_Language
         }
     }
 
-    /// <summary>
-    /// Defines the editor format for the ookQuestion classification type. Text is colored Green
-    /// </summary>
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = "nm_asm_directive")]
     [Name("nm_asm_directive")]
-    //this should be visible to the end user
     [UserVisible(false)]
-    //set the priority to be after the default classifiers
     [Order(Before = Priority.Default)]
     internal sealed class NM_asm_Directive : ClassificationFormatDefinition
     {
-        /// <summary>
-        /// Defines the visual format for the "question" classification type
-        /// </summary>
         public NM_asm_Directive()
         {
             DisplayName = "NM asm Directive"; //human readable version of the name
@@ -50,21 +34,13 @@ namespace NM_asm_Language
         }
     }
 
-    /// <summary>
-    /// Defines the editor format for the ookPeriod classification type. Text is colored Orange
-    /// </summary>
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = "nm_asm_label")]
     [Name("nm_asm_label")]
-    //this should be visible to the end user
     [UserVisible(false)]
-    //set the priority to be after the default classifiers
     [Order(Before = Priority.Default)]
     internal sealed class NM_asm_Label : ClassificationFormatDefinition
     {
-        /// <summary>
-        /// Defines the visual format for the "period" classification type
-        /// </summary>
         public NM_asm_Label()
         {
             DisplayName = " NM asm Label"; //human readable version of the name
@@ -72,21 +48,13 @@ namespace NM_asm_Language
         }
     }
 
-    /// <summary>
-    /// Defines the editor format for the ookPeriod classification type. Text is colored Orange
-    /// </summary>
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = "nm_asm_data_registers")]
     [Name("nm_asm_data_registers")]
-    //this should be visible to the end user
     [UserVisible(false)]
-    //set the priority to be after the default classifiers
     [Order(Before = Priority.Default)]
     internal sealed class NM_asm_Data_registers : ClassificationFormatDefinition
     {
-        /// <summary>
-        /// Defines the visual format for the "period" classification type
-        /// </summary>
         public NM_asm_Data_registers()
         {
             DisplayName = "NM asm Data Registers"; //human readable version of the name
@@ -94,21 +62,13 @@ namespace NM_asm_Language
         }
     }
 
-    /// <summary>
-    /// Defines the editor format for the ookPeriod classification type. Text is colored Orange
-    /// </summary>
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = "nm_asm_comment")]
     [Name("nm_asm_comment")]
-    //this should be visible to the end user
     [UserVisible(false)]
-    //set the priority to be after the default classifiers
     [Order(Before = Priority.Default)]
     internal sealed class NM_asm_Comment : ClassificationFormatDefinition
     {
-        /// <summary>
-        /// Defines the visual format for the "period" classification type
-        /// </summary>
         public NM_asm_Comment()
         {
             DisplayName = "NM asm Comment"; //human readable version of the name
@@ -116,21 +76,13 @@ namespace NM_asm_Language
         }
     }
 
-    /// <summary>
-    /// Defines the editor format for the ookPeriod classification type. Text is colored Orange
-    /// </summary>
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = "nm_asm_quote")]
     [Name("nm_asm_quote")]
-    //this should be visible to the end user
     [UserVisible(false)]
-    //set the priority to be after the default classifiers
     [Order(Before = Priority.Default)]
     internal sealed class NM_asm_Quote : ClassificationFormatDefinition
     {
-        /// <summary>
-        /// Defines the visual format for the "period" classification type
-        /// </summary>
         public NM_asm_Quote()
         {
             DisplayName = "NM asm Quote"; //human readable version of the name
@@ -138,21 +90,13 @@ namespace NM_asm_Language
         }
     }
 
-    /// <summary>
-    /// Defines the editor format for the ookPeriod classification type. Text is colored Orange
-    /// </summary>
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = "nm_asm_number")]
     [Name("nm_asm_number")]
-    //this should be visible to the end user
     [UserVisible(false)]
-    //set the priority to be after the default classifiers
     [Order(Before = Priority.Default)]
     internal sealed class NM_asm_Number : ClassificationFormatDefinition
     {
-        /// <summary>
-        /// Defines the visual format for the "period" classification type
-        /// </summary>
         public NM_asm_Number()
         {
             DisplayName = "NM asm Number"; //human readable version of the name
@@ -160,21 +104,13 @@ namespace NM_asm_Language
         }
     }
 
-    /// <summary>
-    /// Defines the editor format for the ookPeriod classification type. Text is colored Orange
-    /// </summary>
     [Export(typeof(EditorFormatDefinition))]
     [ClassificationType(ClassificationTypeNames = "nm_asm_custom_label")]
     [Name("nm_asm_custom_label")]
-    //this should be visible to the end user
     [UserVisible(false)]
-    //set the priority to be after the default classifiers
     [Order(Before = Priority.Default)]
     internal sealed class NM_asm_Custom_Label: ClassificationFormatDefinition
     {
-        /// <summary>
-        /// Defines the visual format for the "period" classification type
-        /// </summary>
         public NM_asm_Custom_Label()
         {
             DisplayName = "NM asm custom label"; //human readable version of the name
--- Classification/ClassificationType.cs
+++ Classification/ClassificationType.cs
@@ -8,58 +8,34 @@ namespace NM_asm_Language
     {
         #region Type definition
 
-        /// <summary>
-        /// Defines the "ookExclamation" classification type.
-        /// </summary>
         [Export(typeof(ClassificationTypeDefinition))]
         [Name("nm_asm_keyword")]
         internal static ClassificationTypeDefinition NM_asm_Keyword = null;
 
-        /// <summary>
-        /// Defines the "ookQuestion" classification type.
-        /// </summary>
         [Export(typeof(ClassificationTypeDefinition))]
         [Name("nm_asm_directive")]
         internal static ClassificationTypeDefinition NM_asm_Directive = null;
 
-        /// <summary>
-        /// Defines the "ookPeriod" classification type.
-        /// </summary>
         [Export(typeof(ClassificationTypeDefinition))]
         [Name("nm_asm_label")]
         internal static ClassificationTypeDefinition NM_asm_Label = null;
 
-        /// <summary>
-        /// Defines the "ookPeriod" classification type.
-        /// </summary>
         [Export(typeof(ClassificationTypeDefinition))]
         [Name("nm_asm_data_registers")]
         internal static ClassificationTypeDefinition NM_asm_Data_registers = null;
 
-        /// <summary>
-        /// Defines the "ookPeriod" classification type.
-        /// </summary>
         [Export(typeof(ClassificationTypeDefinition))]
         [Name("nm_asm_comment")]
         internal static ClassificationTypeDefinition NM_asm_Comment = null;
 
-        /// <summary>
-        /// Defines the "ookPeriod" classification type.
-        /// </summary>
         [Export(typeof(ClassificationTypeDefinition))]
         [Name("nm_asm_quote")]
         internal static ClassificationTypeDefinition NM_asm_Quote = null;
 
-        /// <summary>
-        /// Defines the "ookPeriod" classification type.
-        /// </summary>
         [Export(typeof(ClassificationTypeDefinition))]
         [Name("nm_asm_number")]
         internal static ClassificationTypeDefinition NM_asm_Number = null;
 
-        /// <summary>
-        /// Defines the "ookPeriod" classification type.
-        /// </summary>
         [Export(typeof(ClassificationTypeDefinition))]
         [Name("nm_asm_custom_label")]
         internal static ClassificationTypeDefinition NM_asm_Custom_Label = null;
