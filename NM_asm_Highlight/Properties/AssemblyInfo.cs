f683d00476fdb245594353ecdbef1c2f2a21939d
MKlimenko <mklimenko29@gmail.com>
2016-08-27 19:32:33 +0300

    Ver 0.3
    	Добавлена русская локализация;
    	Добавлена лицензия использования;
    	Добавлен Changelog.txt

--- NM_asm_Highlight/Properties/AssemblyInfo.cs
+++ NM_asm_Highlight/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("0.2.10.2")]
-[assembly: AssemblyFileVersion("0.2.10.2")]
+[assembly: AssemblyVersion("0.3")]
+[assembly: AssemblyFileVersion("0.3")]
